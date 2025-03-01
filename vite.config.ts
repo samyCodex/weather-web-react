b9265e92306f0028c68cfbdb616c5b2fc6d4cc00
samson onifade <gbemilekeadeboyega@gmail.com>
2025-03-01 13:20:03 +0100

    added currenyId to appSettings

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+        '/api' : 'https://weather-kfmtok34k-samycode3s-projects.vercel.app'
+    }
+  },
   plugins: [react()],
 })
